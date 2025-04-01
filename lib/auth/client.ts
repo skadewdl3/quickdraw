9157f3c4d6b813fd7faf6af9d5e49d5fd296bed4
Soham Karandikar <sohamk10@gmail.com>
2025-04-01 12:11:33 +0530

    Updated sign up logic to use Result type

--- lib/auth/client.ts
+++ lib/auth/client.ts
@@ -1,7 +1,9 @@
-import { createAuthClient } from "better-auth/react";
+import { createAuthClient } from "better-auth/vue";
 
 const config = useRuntimeConfig();
 
-export const authClient = createAuthClient({
+const auth = createAuthClient({
   baseURL: config.public.BETTER_AUTH_URL,
 });
+
+export default auth;
